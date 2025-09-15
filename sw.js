af5192eb908e50072c95407825cee32ac689efb9
motorizedprecision <motorizedprecision@gmail.com>
2025-09-14 19:34:25 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE='mp-lm-roi-30fps-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE='mp-lm-video-load-hotfix-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
