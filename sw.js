c064b9541e8c785f4e891a9f3faa06a4c72fd9b2
motorizedprecision <motorizedprecision@gmail.com>
2025-09-15 15:45:40 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='mp-lm-side-by-side-color-reacquire-v3';
+const CACHE='mp-lm-side-by-side-color-reacquire-v3_1';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
