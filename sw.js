1c825b8d359a1e75dbd7e111253f24687a635222
motorizedprecision <motorizedprecision@gmail.com>
2025-09-14 18:27:51 -0700

    Add files via upload
    Debug tools added

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='mp-lm-tray-fix-v1';
+const CACHE='mp-lm-tray-debug-v1';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
