4f179a8c4cbb447678c2f74be9f16df88c6b0558
motorizedprecision <motorizedprecision@gmail.com>
2025-09-14 18:19:40 -0700

    Add files via upload
    Added checks to Open cv

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-self.addEventListener('fetch', e => {});
+const CACHE='mp-lm-tray-fix-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
