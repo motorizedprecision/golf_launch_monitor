0249a8f182ecbe9967759f5b9a497552f909ac79
motorizedprecision <motorizedprecision@gmail.com>
2025-09-14 19:50:02 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='mp-lm-video-load-hotfix-v2';
+const CACHE='mp-lm-trail-graphics-v1';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
