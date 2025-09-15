2ef70427e1f3c8de87e3a01805c086bf142f5866
motorizedprecision <motorizedprecision@gmail.com>
2025-09-14 18:51:47 -0700

    Add files via upload
    hotfix

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='mp-lm-mac-safari-v1';
+const CACHE='mp-lm-mac-safari-hotfix-v1';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
