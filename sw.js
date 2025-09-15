83e1372c212ccf5d5d07bbf84c70446cec1659d4
motorizedprecision <motorizedprecision@gmail.com>
2025-09-15 14:37:07 -0700

    Add files via upload
    updates

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='mp-lm-side-by-side-nokf-v1';
+const CACHE='mp-lm-side-by-side-nokf-v2';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
